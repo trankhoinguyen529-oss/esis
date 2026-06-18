6446cbae3708720421ac2d3fe9a3d129480dd6d3
trankhoinguyen529-oss <trankhoinguyen529@gmail.com>
2026-06-18 15:34:03 +0700

    Revert "[Rename]"
    This reverts commit ca2f0aa018330a1b615078106938ddecc1fec229.

--- lib/widget/custompiechart.dart
+++ lib/widget/custompiechart.dart
@@ -1,7 +1,7 @@
-import 'package:a_management/data/category_color.dart';
+import 'package:a_management/data/categorycolor.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
-import '../data/pie_chart_data.dart';
+import '../data/piechartdata.dart';
 import '../services/database_service.dart';
 
 /// `BubbleClipper` tạo hình dạng bong bóng đối thoại có mũi tên chỉ sang trái (hướng về biểu đồ).
