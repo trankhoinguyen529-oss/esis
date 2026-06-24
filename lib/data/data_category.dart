54512040ffa4d054df7a1fbe6ae936fd5a92b23d
trankhoinguyen529-oss <trankhoinguyen529@gmail.com>
2026-06-24 21:55:03 +0700

    [DataCategory] Add file

---
+++ lib/data/data_category.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem {
+  final int? id;
+  final int userId;
+  final IconData icon;
+  final String title;
+
+  const CategoryItem({
+    this.id,
+    required this.userId,
+    required this.icon,
+    required this.title,
+  });
+
+  /// Chuyển từ Map (SQLite row) sang TransactionItem
+  factory CategoryItem.fromMap(Map<String, dynamic> map) {
+    return CategoryItem(
+      id: map['id'] as int?,
+      userId: 0,
+      icon: IconData(map['icon_code'] as int, fontFamily: 'MaterialIcons'),
+      title: map['title'] as String,
+    );
+  }
+
+  /// Chuyển TransactionItem sang Map để lưu vào SQLite
+  Map<String, dynamic> toMap() {
+    return {
+      if (id != null) 'id': id,
+      'user_id': userId,
+      'title': title,
+      'icon_code': icon.codePoint,
+    };
+  }
+}
